12b6981375c371666a702dede4a0e2c0aeb1702f
maakhhh <d.makhlonov@mail.ru>
2025-01-13 13:51:09 +0500

    написал ResultTools

--- TagCloudResult/ResultTools/Result.cs
+++ TagCloudResult/ResultTools/Result.cs
@@ -2,5 +2,56 @@
 
 public class Result<T>
 {
-        
+    public T? Value { get; }
+    public string? Error { get; }
+    public bool IsSuccess => Error == null;
+
+    public Result(string? error, T? value = default)
+    {
+        Error = error;
+        Value = value;
+    }
+
+    public T GetValueOrThrow()
+    {
+        if (IsSuccess)
+            return Value!;
+        throw new InvalidOperationException(Error);
+    }
+}
+
+public static class Result
+{
+    public static Result<T> Ok<T>(T value) => new(null, value);
+
+    public static Result<None> Ok() => new(null);
+    
+    public static Result<T> AsResult<T>(this T value) => Ok(value);
+    
+    public static Result<T> Fail<T>(string e) => new(e);
+
+    public static Result<T> Of<T>(Func<T> f, string? error = null)
+    {
+        try
+        {
+            return Ok(f());
+        }
+        catch (Exception e)
+        {
+            return Fail<T>(error ?? e.Message);
+        }
+    }
+
+    public static Result<None> OfAction(Action f, string? error = null)
+    {
+        try
+        {
+            f();
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return Fail<None>(error ?? e.Message);
+        }
+    }
 }
---
+++ TagCloudResult/ResultTools/ResultExtensions.cs
@@ -0,0 +1,42 @@
+﻿namespace ResultTools;
+
+public static class ResultExtensions
+{
+    public static Result<TOutput> Then<TInput, TOutput>(
+        this Result<TInput> input,
+        Func<TInput, TOutput> continuation)
+        => input.Then(inp => Result.Of(() => continuation(inp)));
+    
+    public static Result<None> Then<TInput>(
+        this Result<TInput> input,
+        Action<TInput> continuation)
+    => input.Then(inp => Result.OfAction(() => continuation(inp)));
+
+    public static Result<TOutput> Then<TInput, TOutput>(
+        this Result<TInput> input,
+        Func<TInput, Result<TOutput>> continuation)
+        => input.IsSuccess
+            ? continuation(input.Value!)
+            : Result.Fail<TOutput>(input.Error!);
+
+    public static Result<TInput> OnFail<TInput>(
+        this Result<TInput> input,
+        Action<string> handleError)
+    {
+        if (!input.IsSuccess)
+            handleError(input.Error!);
+        return input;
+    }
+
+    public static Result<TInput> ReplaceError<TInput>(
+        this Result<TInput> input,
+        Func<string, string> replaceError)
+        => input.IsSuccess
+            ? input
+            : Result.Fail<TInput>(replaceError(input.Error!));
+
+    public static Result<TInput> RefineError<TInput>(
+        this Result<TInput> input,
+        string error)
+        => input.ReplaceError(err => $"{error}. {err}");
+}
